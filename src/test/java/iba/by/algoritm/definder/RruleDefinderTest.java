40a17c2854879adfc376c875270a963eed820619
ShakhraiR <shahrai.robert@gmail.com>
2018-11-23 13:41:29 +0300

    delete comments

--- src/test/java/iba/by/algoritm/definder/RruleDefinderTest.java
+++ src/test/java/iba/by/algoritm/definder/RruleDefinderTest.java
@@ -20,44 +20,44 @@ public class RruleDefinderTest {
         rruleDefinder = new RruleDefinder(rrule);
     }
 
-//    @Test
-//    public void shouldReturnFreqWeeklyAndIntervalTwo() {
-//        //given
-//        Data.getFirstEvents().forEach(x -> eventHandler.addEvent(x));
-//        eventHandler.sortEvents();
-//        //when
-//        rruleDefinder.defineRruleExDates(eventHandler.getEvents());
-//        //then
-//        Assert.assertEquals(rrule.getInterval(), Data.INTERVAL_FOR_FIRST_DATES);
-//        Assert.assertEquals(rrule.getRruleFreqType(), Data.FREQ_FOR_FIRST_DATES);
-//        Assert.assertEquals(rrule.getExDates().size(), Data.AMOUNT_OF_DATES_TO_EXDATE_FOR_FIRST_DATES);
-//    }
-//
-//    @Test
-//    public void shouldReturnFreqDailyAndIntervalTwo() {
-//        //given
-//        Data.getSecondEvents().forEach(x -> eventHandler.addEvent(x));
-//        eventHandler.sortEvents();
-//        //when
-//        rruleDefinder.defineRruleExDates(eventHandler.getEvents());
-//        //then
-//        Assert.assertEquals(rrule.getInterval(), Data.INTERVAL_FOR_SECOND_DATES);
-//        Assert.assertEquals(rrule.getRruleFreqType(), Data.FREQ_FOR_FSECOND_DATES);
-//        Assert.assertEquals(rrule.getExDates().size(), Data.AMOUNT_OF_DATES_TO_EXDATE_FOR_SECOND_DATES);
-//    }
-//
-//    @Test
-//    public void shouldReturnFreqHourlyAndIntervalThree() {
-//        //given
-//        Data.getThirdEvents().forEach(x -> eventHandler.addEvent(x));
-//        eventHandler.sortEvents();
-//        //when
-//        rruleDefinder.defineRruleExDates(eventHandler.getEvents());
-//        //then
-//        Assert.assertEquals(rrule.getInterval(), Data.INTERVAL_FOR_THIRD_DATES);
-//        Assert.assertEquals(rrule.getRruleFreqType(), Data.FREQ_FOR_THIRD_DATES);
-//        Assert.assertEquals(rrule.getExDates().size(), Data.AMOUNT_OF_DATES_TO_EXDATE_FOR_THIRD_DATES);
-//    }
+    @Test
+    public void shouldReturnFreqWeeklyAndIntervalTwo() {
+        //given
+        Data.getFirstEvents().forEach(x -> eventHandler.addEvent(x));
+        eventHandler.sortEvents();
+        //when
+        rruleDefinder.defineRruleExDates(eventHandler.getEvents());
+        //then
+        Assert.assertEquals(rrule.getInterval(), Data.INTERVAL_FOR_FIRST_DATES);
+        Assert.assertEquals(rrule.getRruleFreqType(), Data.FREQ_FOR_FIRST_DATES);
+        Assert.assertEquals(rrule.getExDates().size(), Data.AMOUNT_OF_DATES_TO_EXDATE_FOR_FIRST_DATES);
+    }
+
+    @Test
+    public void shouldReturnFreqDailyAndIntervalTwo() {
+        //given
+        Data.getSecondEvents().forEach(x -> eventHandler.addEvent(x));
+        eventHandler.sortEvents();
+        //when
+        rruleDefinder.defineRruleExDates(eventHandler.getEvents());
+        //then
+        Assert.assertEquals(rrule.getInterval(), Data.INTERVAL_FOR_SECOND_DATES);
+        Assert.assertEquals(rrule.getRruleFreqType(), Data.FREQ_FOR_FSECOND_DATES);
+        Assert.assertEquals(rrule.getExDates().size(), Data.AMOUNT_OF_DATES_TO_EXDATE_FOR_SECOND_DATES);
+    }
+
+    @Test
+    public void shouldReturnFreqHourlyAndIntervalThree() {
+        //given
+        Data.getThirdEvents().forEach(x -> eventHandler.addEvent(x));
+        eventHandler.sortEvents();
+        //when
+        rruleDefinder.defineRruleExDates(eventHandler.getEvents());
+        //then
+        Assert.assertEquals(rrule.getInterval(), Data.INTERVAL_FOR_THIRD_DATES);
+        Assert.assertEquals(rrule.getRruleFreqType(), Data.FREQ_FOR_THIRD_DATES);
+        Assert.assertEquals(rrule.getExDates().size(), Data.AMOUNT_OF_DATES_TO_EXDATE_FOR_THIRD_DATES);
+    }
 
     @Test
     public void shouldReturnFreqMinutelyAndIntervalTwentyFive() {
